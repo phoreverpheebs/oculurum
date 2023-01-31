b0590ff175367178fc12dbebf0716a337776e24a
phoreverpheebs <phoreverphoebs@proton.me>
2023-01-31 17:24:55 +0900

    Better error handling (removed unwraps) and better padding calculations

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
+use bitflags::bitflags;
+use png;
 use std::{
-    env,
-    process,
-    fs,
+    env, fs,
     io::{self, Read, Write},
     path::{Path, PathBuf},
+    process,
 };
-use png;
-use bitflags::bitflags;
 
-bitflags!{
+bitflags! {
     #[derive(Default)]
     struct Flags: u32 {
         const C_DEFAULT     = 1 << 2;
@@ -33,7 +32,7 @@ struct ParseFlagError;
 impl Flags {
     #[allow(deprecated)]
     fn compression(&self) -> png::Compression {
-        let  n = *self & Flags::COMPRESSION;
+        let n = *self & Flags::COMPRESSION;
         match n {
             Flags::C_FAST => png::Compression::Fast,
             Flags::C_BEST => png::Compression::Best,
@@ -43,7 +42,7 @@ impl Flags {
         }
     }
 
-    fn compression_from_str(s: &str) ->  Result<Self, ParseFlagError> {
+    fn compression_from_str(s: &str) -> Result<Self, ParseFlagError> {
         s.parse::<u32>().map_or(Err(ParseFlagError), |n| match n {
             0 => Ok(Flags::C_DEFAULT),
             1 => Ok(Flags::C_FAST),
@@ -86,54 +85,57 @@ fn main() {
     while let Some(arg) = args.pop() {
         if arg.starts_with("--") {
             match arg.as_str() {
-                "--compression" => if let Some(value) = args.pop() {
-                    flags |= Flags::compression_from_str(&value)
-                        .unwrap_or_else(|_| {
+                "--compression" => {
+                    if let Some(value) = args.pop() {
+                        flags |= Flags::compression_from_str(&value).unwrap_or_else(|_| {
                             eprintln!("Invalid argument to `--compression`.\nUsing default.");
                             Flags::C_DEFAULT
                         });
-                } else {
-                    exit_with_help(1);
-                },
-                "--type" => if let Some(value) = args.pop() {
-                    flags |= Flags::type_from_str(&value)
-                        .unwrap_or_else(|_| {
+                    } else {
+                        exit_with_help(1);
+                    }
+                }
+                "--type" => {
+                    if let Some(value) = args.pop() {
+                        flags |= Flags::type_from_str(&value).unwrap_or_else(|_| {
                             eprintln!("Invalid argument to `--type`.\nUsing default.");
                             Flags::T_GRAY
                         });
-                } else {
-                    exit_with_help(1);
-                },
+                    } else {
+                        exit_with_help(1);
+                    }
+                }
                 "--help" => exit_with_help(0),
                 _ => exit_with_help(1),
             }
         } else if arg.starts_with("-") {
             match arg.as_str() {
-                "-c" => if let Some(value) = args.pop() {
-                    flags |= Flags::compression_from_str(&value)
-                        .unwrap_or_else(|_| {
+                "-c" => {
+                    if let Some(value) = args.pop() {
+                        flags |= Flags::compression_from_str(&value).unwrap_or_else(|_| {
                             eprintln!("Invalid argument to `-c`.\nUsing default.");
                             Flags::C_DEFAULT
                         });
-                } else {
-                    exit_with_help(1);
-                },
-                "-t" => if let Some(value) = args.pop() {
-                    flags |= Flags::type_from_str(&value)
-                        .unwrap_or_else(|_| {
+                    } else {
+                        exit_with_help(1);
+                    }
+                }
+                "-t" => {
+                    if let Some(value) = args.pop() {
+                        flags |= Flags::type_from_str(&value).unwrap_or_else(|_| {
                             eprintln!("Invalid argument to `-t`.\nUsing default.");
                             Flags::T_GRAY
                         });
-                } else {
-                    exit_with_help(1);
-                },
+                    } else {
+                        exit_with_help(1);
+                    }
+                }
                 "-h" => exit_with_help(0),
                 _ => exit_with_help(1),
             }
         } else {
             if !input_file.is_empty() {
-                eprintln!("Multiple input files.");
-                process::exit(2);
+                eprint_and_exit("Multiple input files.\n", 2);
             }
 
             input_file = arg;
@@ -155,16 +157,18 @@ const WHITE_PX: u8 = 0xff;
 
 #[inline]
 fn handle_bitwise(data: Vec<u8>) -> Vec<u8> {
-    data
-        .into_iter()
-        .map(|b| (0..8)
-            .map(|shift| if b & 1 << shift == 0 {
-                BLACK_PX
-            } else {
-                WHITE_PX
-            })
-            .collect::<Vec<u8>>()
-        )
+    data.into_iter()
+        .map(|b| {
+            (0..8)
+                .map(|shift| {
+                    if b & 1 << shift == 0 {
+                        BLACK_PX
+                    } else {
+                        WHITE_PX
+                    }
+                })
+                .collect::<Vec<u8>>()
+        })
         .flatten()
         .collect::<Vec<u8>>()
 }
@@ -193,20 +197,43 @@ fn run<P: AsRef<Path>>(path: P, flags: Flags) -> Result<(), png::EncodingError> 
 
     if path.is_dir() {
         filenames = Vec::new();
-        dimension = f64::sqrt(multiplier * calculate_dimensions(&path, &mut filenames)
-            .expect("Error in calculating  directory size.") as f64) as u32 + 1;
+        dimension = f64::sqrt(
+            multiplier
+                * calculate_dimensions(&path, &mut filenames)
+                    .expect("Error in calculating  directory size.") as f64,
+        ) as u32
+            + 1;
     } else {
-        dimension = f64::sqrt(multiplier * path
-            .metadata()
-            .expect("Couldn't get file metadata.")
-            .len() as f64) as u32 + 1;
+        dimension = f64::sqrt(
+            multiplier * path.metadata().expect("Couldn't get file metadata.").len() as f64,
+        ) as u32
+            + 1;
         filenames = Vec::with_capacity(1);
         filenames.push(path.to_path_buf());
     }
 
-    let mut output_path = path.to_str().unwrap().to_string();
-    output_path.push_str(".png");
-    let file = fs::File::create(output_path).unwrap();
+    const DEFAULT_OUTPUT: &str = "oculurised.png";
+
+    let mut output_path = match path.to_str() {
+        Some(out) => out.trim_end_matches(['/', '\\']).split(['/', '\\']).last().unwrap_or(DEFAULT_OUTPUT).to_string(),
+        None => eprint_and_exit(
+            "Output path couldn't be validated (invalid characters)\n",
+            5,
+        ),
+    };
+    
+    output_path.push_str(if output_path.is_empty() {
+        DEFAULT_OUTPUT
+    } else {
+        ".png"
+    });
+    let file = match fs::File::create(output_path) {
+        Ok(f) => f,
+        Err(e) => eprint_and_exit(
+            &format!("Output file couldn't be created with error: {e}\n"),
+            4,
+        ),
+    };
 
     let ref mut w = io::BufWriter::new(file);
     let mut encoder = png::Encoder::new(w, dimension, dimension);
@@ -220,39 +247,79 @@ fn run<P: AsRef<Path>>(path: P, flags: Flags) -> Result<(), png::EncodingError> 
         handle_others
     };
 
-    let mut binding = encoder.write_header().unwrap();
-    let mut writer = binding.stream_writer().unwrap();
+    // fails if width or height are 0
+    // or if the colour type // bit depth combination isn't valid
+    let mut binding = match encoder.write_header() {
+        Ok(w) => w,
+        Err(e) => eprint_and_exit(
+            &format!("Failed initialising PNG writer, due to encoder error: {e}\n"),
+            6,
+        ),
+    };
+    let mut writer = match binding.stream_writer() {
+        Ok(w) => w,
+        Err(e) => eprint_and_exit(
+            &format!("Failed initialising PNG stream writer, due to encoder error: {e}\n"),
+            7,
+        ),
+    };
 
     let mut buffer: [u8; 4096] = [0; 4096];
     let mut written = 0usize;
 
-    filenames
-        .into_iter()
-        .for_each(|filename| {
-            eprintln!("Writing data from {}", &filename.display());
-            let ref mut f = fs::File::open(filename).unwrap();
-            let mut reader = io::BufReader::new(f);
-
-            let mut nread = reader.read(&mut buffer).unwrap();
-
-            let mut chunk_counter = 0;
-
-            while nread > 0 {
-                eprint!("Writing chunk {chunk_counter}\r");
-
-                writer.write_all(&handler(buffer[..nread].to_vec())).unwrap();
-                written += nread;
-
-                nread = reader.read(&mut buffer).unwrap();
-                chunk_counter += 1;
+    // normal for loop instead of Vec.for_each, allows us to use `continue`
+    for filename in filenames.into_iter() {
+        eprintln!("Writing data from {}", &filename.display());
+        let ref mut f = match fs::File::open(filename) {
+            Ok(f) => f,
+            Err(e) => {
+                eprintln!("Couldn't open file with error: {e}\n");
+                continue;
             }
-        });
+        };
+        let mut reader = io::BufReader::new(f);
 
-    let padding = vec![0;
-        (((bytes_per_pixel << 2).checked_mul(dimension.checked_pow(2).unwrap()).unwrap()) as usize)
-            .checked_sub(written)
-            .unwrap_or(0)
-    ];
+        let mut nread = match reader.read(&mut buffer) {
+            Ok(n) => n,
+            Err(_) => {
+                // reader or buffer are blocking, buffer is unlikely,
+                // so we'll just continue to move onto the next file
+                // aka the next reader
+                continue;
+            }
+        };
+
+        let mut chunk_counter = 0;
+
+        while nread > 0 {
+            eprint!("Writing chunk {chunk_counter}\r");
+
+            // honestly this should most likely not error with normal circumstances
+            // and if it does it's most likely an OS blocking, which would not be
+            // resolved by retrying
+            writer
+                .write_all(&handler(buffer[..nread].to_vec()))
+                .unwrap();
+            written += nread;
+
+            nread = match reader.read(&mut buffer) {
+                Ok(n) => n,
+                // same as before
+                Err(_) => break,
+            };
+            chunk_counter += 1;
+        }
+    }
+
+    // multiply dimension * dimension by bytes_per_pixel to get
+    // the amount of bytes needed to write the full image
+    // then subtract the amount of written bytes
+    let padding_len: usize = (bytes_per_pixel as usize)
+        .overflowing_mul((dimension as usize).overflowing_pow(2).0).0
+        .checked_sub(written)
+        .unwrap_or(0);
+
+    let padding = vec![0; padding_len];
     writer.write_all(&padding[..]).unwrap();
     Ok(())
 }
@@ -275,8 +342,9 @@ fn calculate_dimensions<P: AsRef<Path>>(dir: P, filenames: &mut Vec<PathBuf>) ->
     Ok(size)
 }
 
-fn exit_with_help(code: u8) {
-    println!("Usage: oculurum [options] <file or directory>
+fn exit_with_help(code: i32) -> ! {
+    println!(
+        "Usage: oculurum [options] <file or directory>
 
 Options:
         -h | --help
@@ -302,6 +370,14 @@ Options:
                 5 => RGB Alpha
             Unimplemented:
                 _ => Indexed 
-");
-    process::exit(code as i32);
+"
+    );
+    process::exit(code)
+}
+
+#[inline]
+fn eprint_and_exit(s: &str, code: i32) -> ! {
+    // unwrapping here is kinda pointless, since it's a noreturn function anyway
+    let _ = io::stderr().write_all(s.as_bytes());
+    process::exit(code)
 }
